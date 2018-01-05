043acc7e0ae7c53d1ea32a6a9a84ffebcf521098
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-01-05 13:38:01 -0800

    All getter method tests fail

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,20 +1,42 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Event {
-    private Map<String, String> hashMapEventProps;
-    private int intNumberOfGuests;
+    private Map<String, String> hashMapEventPrompts;
     private List<String> listFoodChoices;
     private List<String> listBeverageChoices;
-    private List<String> listEntertainment;
+    private List<String> listEntertainmentChoices;
+    private int intNumberOfGuests;
 
-    public Event(int intNumberOfGuests, List<String> listFoodChoices, List<String> listBeverageChoices, List<String> listEntertainment) {
-
-
+    public Event(int intNumberOfGuests, List<String> listFoodChoices, List<String> listBeverageChoices, List<String> listEntertainmentChoices) {
+        this.hashMapEventPrompts = new HashMap<>();
+        this.intNumberOfGuests = intNumberOfGuests;
+        this.listFoodChoices = listFoodChoices;
+        this.listBeverageChoices = listBeverageChoices;
+        this.listEntertainmentChoices = listEntertainmentChoices;
     }
+
+    // Getter Methods
+    public Map<String, String> getHashMapEventPrompts() {
+        return null;
+    }
+    public List<String> getListFoodChoices() {
+        return null;
+    }
+    public List<String> getListBeverageChoices() {
+        return null;
+    }
+    public List<String> getListEntertainmentChoices() {
+        return null;
+    }
+    public int getIntNumberOfGuests() {
+        return 0;
+    }
+    // Setter Methods
 
 }
 
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -4,8 +4,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
-
+import java.util.HashMap;
 
 public class EventTest {
     // Instantiates Correctly
@@ -24,4 +25,102 @@ public class EventTest {
         Event testEvent = new Event(10, testFoodChoices, testBeverageChoices, testEntertainmentChoices);
         assertEquals(true, testEvent instanceof Event);
     }
+
+    // Getter Methods
+    @Test
+    public void getHashMapEventPrompts_getsHashMapEventPrompts_String() {
+        Map<String, String> hashMapEventPrompts = new HashMap<>();
+        List<String> testFoodChoices = new ArrayList<>();
+        List<String> testBeverageChoices = new ArrayList<>();
+        List<String> testEntertainmentChoices = new ArrayList<>();
+        hashMapEventPrompts.put("How many guests will be attending?", "30");
+        testFoodChoices.add("Cake");
+        testFoodChoices.add("Lasagna");
+        testBeverageChoices.add("Soda");
+        testBeverageChoices.add("Wine");
+        testEntertainmentChoices.add("Live band");
+        testEntertainmentChoices.add("Clown");
+
+        Event testEvent = new Event(10, testFoodChoices, testBeverageChoices, testEntertainmentChoices);
+        assertEquals("30", testEvent.getHashMapEventPrompts());
+    }
+    @Test
+    public void getListFoodChoices_getsListFoodChoices_ArrayList() {
+        Map<String, String> hashMapEventPrompts = new HashMap<>();
+        List<String> testFoodChoices = new ArrayList<>();
+        List<String> testBeverageChoices = new ArrayList<>();
+        List<String> testEntertainmentChoices = new ArrayList<>();
+        List<String> listExpected = new ArrayList<>();
+        hashMapEventPrompts.put("How many guests will be attending?", "30");
+        testFoodChoices.add("Cake");
+        testFoodChoices.add("Lasagna");
+        testBeverageChoices.add("Soda");
+        testBeverageChoices.add("Wine");
+        testEntertainmentChoices.add("Live band");
+        testEntertainmentChoices.add("Clown");
+
+        Event testEvent = new Event(10, testFoodChoices, testBeverageChoices, testEntertainmentChoices);
+        listExpected.add("Cake");
+        listExpected.add("Lasagna");
+        assertEquals(listExpected, testEvent.getListFoodChoices());
+    }
+    @Test
+    public void getListBeverageChoices_getsListBeverageChoices_ArrayList() {
+        Map<String, String> hashMapEventPrompts = new HashMap<>();
+        List<String> testFoodChoices = new ArrayList<>();
+        List<String> testBeverageChoices = new ArrayList<>();
+        List<String> testEntertainmentChoices = new ArrayList<>();
+        List<String> listExpected = new ArrayList<>();
+        hashMapEventPrompts.put("How many guests will be attending?", "30");
+        testFoodChoices.add("Cake");
+        testFoodChoices.add("Lasagna");
+        testBeverageChoices.add("Soda");
+        testBeverageChoices.add("Wine");
+        testEntertainmentChoices.add("Live band");
+        testEntertainmentChoices.add("Clown");
+
+        Event testEvent = new Event(10, testFoodChoices, testBeverageChoices, testEntertainmentChoices);
+        listExpected.add("Soda");
+        listExpected.add("Wine");
+        assertEquals(listExpected, testEvent.getListBeverageChoices());
+    }
+    @Test
+    public void getListEntertainmentChoices_getsListEntertainmentChoices_ArrayList() {
+        Map<String, String> hashMapEventPrompts = new HashMap<>();
+        List<String> testFoodChoices = new ArrayList<>();
+        List<String> testBeverageChoices = new ArrayList<>();
+        List<String> testEntertainmentChoices = new ArrayList<>();
+        List<String> listExpected = new ArrayList<>();
+        hashMapEventPrompts.put("How many guests will be attending?", "30");
+        testFoodChoices.add("Cake");
+        testFoodChoices.add("Lasagna");
+        testBeverageChoices.add("Soda");
+        testBeverageChoices.add("Wine");
+        testEntertainmentChoices.add("Live band");
+        testEntertainmentChoices.add("Clown");
+
+        Event testEvent = new Event(10, testFoodChoices, testBeverageChoices, testEntertainmentChoices);
+        listExpected.add("Live band");
+        listExpected.add("Clown");
+        assertEquals(listExpected, testEvent.getListEntertainmentChoices());
+    }
+    @Test
+    public void getIntNumberOfGuests_getsIntNumberOfGuests_int() {
+        Map<String, String> hashMapEventPrompts = new HashMap<>();
+        List<String> testFoodChoices = new ArrayList<>();
+        List<String> testBeverageChoices = new ArrayList<>();
+        List<String> testEntertainmentChoices = new ArrayList<>();
+        int listExpected = 10;
+        hashMapEventPrompts.put("How many guests will be attending?", "30");
+        testFoodChoices.add("Cake");
+        testFoodChoices.add("Lasagna");
+        testBeverageChoices.add("Soda");
+        testBeverageChoices.add("Wine");
+        testEntertainmentChoices.add("Live band");
+        testEntertainmentChoices.add("Clown");
+
+        Event testEvent = new Event(10, testFoodChoices, testBeverageChoices, testEntertainmentChoices);
+        assertEquals(listExpected, testEvent.getIntNumberOfGuests());
+    }
+
 }
