9df2e801be270900503f7b141a1021cafe180c3e
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-01-05 13:12:55 -0800

    Instantiation test fails

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,16 +1,17 @@
 package models;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Event {
     private Map<String, String> hashMapEventProps;
     private int intNumberOfGuests;
-    private String stringFoodType;
-    private String stringBeverageType;
-    private String stringEntertainment;
+    private List<String> listFoodChoices;
+    private List<String> listBeverageChoices;
+    private List<String> listEntertainment;
 
-    public Event(int intNumberOfGuests, String stringFoodType, String stringBeverageType, String stringEntertainment) {
+    public Event(int intNumberOfGuests, List<String> listFoodChoices, List<String> listBeverageChoices, List<String> listEntertainment) {
 
 
     }
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -3,6 +3,25 @@ package models;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class EventTest {
+import java.util.List;
+import java.util.ArrayList;
 
+
+public class EventTest {
+    // Instantiates Correctly
+    @Test
+    public void newEvent_instantiatesCorrectly() {
+        List<String> testFoodChoices = new ArrayList<>();
+        List<String> testBeverageChoices = new ArrayList<>();
+        List<String> testEntertainmentChoices = new ArrayList<>();
+        testFoodChoices.add("Cake");
+        testFoodChoices.add("Lasagna");
+        testBeverageChoices.add("Soda");
+        testBeverageChoices.add("Wine");
+        testEntertainmentChoices.add("Live band");
+        testEntertainmentChoices.add("Clown");
+
+        Event testEvent = new Event(10, testFoodChoices, testBeverageChoices, testEntertainmentChoices);
+        assertEquals(false, testEvent instanceof Event);
+    }
 }
