1c29bd53c67bec81b37187b78324c1b81402d1aa
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2018-01-05 15:02:31 -0800

    Added coupon enum types

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,41 @@
 import models.Event;
+import models.ConsoleUI;
+
+import java.io.IOException;
 
 public class App {
+    public static void main(String[] args) throws IOException {
+        ConsoleUI console = new ConsoleUI();
+        boolean quit = false;
+
+        console.startMenuPrompt();
+
+        while (!quit) {
+            int selection = console.getStartMenuPromptInput();
+
+            switch (selection) {
+                case 1:
+                    console.weddingPrompt();
+                    break;
+                case 2:
+                    console.birthdayPrompt();
+                    break;
+                case 3:
+                    console.reunionPrompt();
+                    break;
+                case 4:
+                    console.otherPrompt();
+                    break;
+                case 5:
+                    quit = true;
+                    break;
+                default:
+                    console.displayPrompt("What?");
+                    break;
+            }
+        }
+
+
+
+    }
 }
