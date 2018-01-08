defb5e85407c677caafb994e49bbf639ca9fdc54
Abdullah Yahya <abdullah@>
2018-01-08 12:02:13 -0800

    Initial Commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,48 +1,68 @@
 import models.*;
 
-import java.io.IOException;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static  spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) throws IOException {
-        ConsoleUI console = new ConsoleUI();
-        boolean quit = false;
+    public static void main(String[] args) {
+        // Static files e.g., Stylesheets, Scripts, Images, etc.
+        staticFileLocation("/public");
 
-        try {
-            console.startMenuPrompt();
-            while (!quit) {
-                int selection = console.getStartMenuPromptInput();
+        // Root Route
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "index.hbs");
+        }, new HandlebarsTemplateEngine());
+        get("/planner", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "planner.hbs");
+        }, new HandlebarsTemplateEngine());
 
-                switch (selection) {
-                    case 1:
-                        console.weddingPrompt();
-                        break;
-                    case 2:
-                        console.birthdayPrompt();
-                        break;
-                    case 3:
-                        console.reunionPrompt();
-                        break;
-                    case 4:
-                        try {
-                            console.otherPrompt();
-                        } catch (NumberFormatException e) {
-                            console.invalidResponse();
-                            console.otherPrompt();
-                        }
-                        break;
-                    case 5:
-                        quit = true;
-                        console.displayPrompt("Thank you for visiting us at Abdul's Event Planner");
-                        console.displayPrompt("Goodbye.");
-                        break;
-                    default:
-                        console.invalidResponse();
-                        break;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+//        ConsoleUI console = new ConsoleUI();
+//        boolean quit = false;
+//
+//        try {
+//            console.startMenuPrompt();
+//            while (!quit) {
+//                int selection = console.getStartMenuPromptInput();
+//
+//                switch (selection) {
+//                    case 1:
+//                        console.weddingPrompt();
+//                        break;
+//                    case 2:
+//                        console.birthdayPrompt();
+//                        break;
+//                    case 3:
+//                        console.reunionPrompt();
+//                        break;
+//                    case 4:
+//                        try {
+//                            console.otherPrompt();
+//                        } catch (NumberFormatException e) {
+//                            console.invalidResponse();
+//                            console.otherPrompt();
+//                        }
+//                        break;
+//                    case 5:
+//                        quit = true;
+//                        console.displayPrompt("Thank you for visiting us at Abdul's Event Planner");
+//                        console.displayPrompt("Goodbye.");
+//                        break;
+//                    default:
+//                        console.invalidResponse();
+//                        break;
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
